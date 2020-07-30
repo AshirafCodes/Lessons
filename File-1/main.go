b69e5462662694cd1c3b3ea7ce70342a6940ac3a
Ashiraf Kaaba <AshirafCodes>
2020-07-29 23:52:27 -0400

    File-1 API main.go

---
+++ File-1/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Start API")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Bulungi")
+		w.WriteHeader(200)
+		w.Write([]byte("This path is /"))
+	})
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Bulungi")
+		w.WriteHeader(200)
+		w.Write([]byte("This path is /home"))
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
