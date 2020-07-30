40b30d64cd10761d45ec0dd154502ca563534251
Ashiraf Kaaba <AshirafCodes>
2020-07-30 02:29:40 -0400

    about

--- File-API-V1.0.1/main.go
+++ File-API-V1.0.1/main.go
@@ -23,5 +23,10 @@ func main() {
 		w.WriteHeader(200)
 		w.Write([]byte("This path is /info"))
 	})
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Bulungi")
+		w.WriteHeader(200)
+		w.Write([]byte("This path is /about"))
+	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
