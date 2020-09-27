8b4dfb2c08e71216f431193dc7500bf8e8d91b71
William Heiderman <heiderman.william@gmail.com>
2020-09-27 12:45:13 -0400

    homework 4 review

--- Homework4/homework4.go
+++ Homework4/homework4.go
@@ -5,15 +5,22 @@ import "fmt"
 func main() {
 	// Homework 1 Boolean and Trip
 	// Booking Trip
-	// Decesion on Trip
+	// Decision on Trip
 	// Who are going on trip
 	Frank := true
 	Pascle := true
-	Anita := true
+	Anita := false
+	// If Frank, Pascle, or Anita want to go on the trip then they go on the trip
 	fmt.Println("Going for trip?", Frank || Pascle || Anita)
+	fmt.Println("Is Frank going on the trip?", Frank)
+	fmt.Println("Is Pascle going on the trip?", Pascle)
+	fmt.Println("Is Anita going on the trip?", Anita)
+	fmt.Println("Are Anita AND Pascle going on the trip?", Anita && Pascle)
+	fmt.Println("Are Anita AND Frank going on the trip?", Anita && Frank)
+	fmt.Println("Is Anita or Frank going on the trip?", Anita || Frank)
 
-	// Number: Intergers and floats.
-	// Integers = Whole numebers
+	// Number: Integers and floats.
+	// Integers = Whole numbers
 	// Floats = decimal numbers
 	// my math operators: + - * / % ()
 	// % = remainder
@@ -32,6 +39,7 @@ func main() {
 	fmt.Println("4-4 =", 4-4)
 	fmt.Println("4%4 =", 4%4)
 	fmt.Println("7%3 =", 7%3)
+	fmt.Println("8%6 =", 8%6)
 
 	// Leaving in space
 	fmt.Println("")
@@ -44,7 +52,8 @@ func main() {
 	fmt.Println("2.0+5 =", 2.0+5)
 	fmt.Println("20.0/7 =", 20.0/7)
 	fmt.Println("15*3.0 =", 15*3.0)
-	fmt.Println("30-11 =", 30-11)
+	fmt.Println("30.63-11.34 =", 30.63-11.34)
+	fmt.Println("1.23+3.33 =", 1.23+3.33) // 4.56
 
 	// Leaving in space
 	fmt.Println("")
@@ -62,7 +71,7 @@ func main() {
 	iowa := true
 	minnesota := true
 	arizona := false
-	fmt.Println("Wost states in America?", arizona && iowa && minnesota)
+	fmt.Println("Worst states in America?", arizona && iowa && minnesota)
 	fmt.Println("Is the mentioned among the best?", iowa || minnesota)
 	fmt.Println("Arizona is best, right?", iowa && minnesota && arizona)
 
@@ -76,13 +85,13 @@ func main() {
 	stringT := ""
 
 	fmt.Println("")
-	
+
 	fmt.Println("StringN + stringO =", stringN+stringO)
 	stringT = stringN + stringO
 	fmt.Println("")
 	fmt.Println("StringN =", stringN)
 	fmt.Println("StringO =", stringO)
 	fmt.Println("StringT =", stringT)
-	
+
 	fmt.Println("")
 }
